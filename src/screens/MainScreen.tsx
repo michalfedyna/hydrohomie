3a493ddd6a9e80b3b28946cdf46efff79ea0f61e
michalfedyna <michal@fedyna.pl>
2024-06-16 16:30:55 +0200

    feat: add e2e

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Button, StyleSheet} from 'react-native';
 import {Text, View} from '@components';
 import type {Screen} from '@types';
 import type {MainStackScreenProps} from '@navigation';
 
-const MainScreen: Screen<MainStackScreenProps<'Main'>> = () => {
+const MainScreen: Screen<MainStackScreenProps<'Main'>> = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Hello Main!</Text>
+      <Button
+        title="Click Me!"
+        onPress={() => {
+          navigation.navigate('Settings');
+        }}
+      />
     </View>
   );
 };
